e9d15a329eb19e919e113b76dd21cb8fc47682c6
Louis Forestier <louis.forestier@etu.univ-poitiers.fr>
2022-02-10 00:24:22 +0100

    avancée tp4 a3d fonctionne sur mobile, le probleme venait des objets avec une normal map fausse tentative d'ajout des normals avec l'objimporter, ne fonctionne pas

--- A3D/tp1/app/src/main/java/fr/univ_poitiers/dptinfo/algo3d/OBJImporter.java
+++ A3D/tp1/app/src/main/java/fr/univ_poitiers/dptinfo/algo3d/OBJImporter.java
@@ -1,11 +1,15 @@
 package fr.univ_poitiers.dptinfo.algo3d;
 
+import android.util.Log;
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Set;
+import java.util.TreeSet;
 
 public class OBJImporter {
 
@@ -13,6 +17,7 @@ public class OBJImporter {
         List<Float> verticesList = new ArrayList<>();
         List<Float> normalsList = new ArrayList<>();
         List<Integer> trianglesList = new ArrayList<>();
+        Set<Integer> trianglesNormalsSet= new TreeSet<>();
         BufferedReader reader = new BufferedReader(new InputStreamReader(stream));
         String lineText;
         try {
@@ -38,6 +43,11 @@ public class OBJImporter {
                             trianglesList.add(Integer.parseInt((data[1].split("/"))[0]));
                             trianglesList.add(Integer.parseInt((data[2].split("/"))[0]));
                             trianglesList.add(Integer.parseInt((data[3].split("/"))[0]));
+                            if (data[1].split("/").length > 2) {
+                                trianglesNormalsSet.add(Integer.parseInt((data[1].split("/"))[2]));
+                                trianglesNormalsSet.add(Integer.parseInt((data[2].split("/"))[2]));
+                                trianglesNormalsSet.add(Integer.parseInt((data[3].split("/"))[2]));
+                            }
                             break;
                         default:
                     }
@@ -46,7 +56,6 @@ public class OBJImporter {
         } catch (IOException e) {
             e.printStackTrace();
         }
-
         float[] vertexpos = new float[verticesList.size()];
         for (int i = 0 ; i < verticesList.size() ; i++){
             vertexpos[i] = verticesList.get(i);
@@ -55,10 +64,20 @@ public class OBJImporter {
         for (int i = 0 ; i < trianglesList.size(); i++){
             triangles[i] = trianglesList.get(i)-1;
         }
-        float[] normals = new float[normalsList.size()];
-        for (int i = 0 ; i < normalsList.size() ; i++){
-            normals[i] = normalsList.get(i);
-        }
+
+        float[] normals;
+        /*if (normalsList.size()>0) {
+            Log.d("OBJIMPORT","normalslist size"+normalsList.size());
+            Log.d("OBJIMPORT","trianglesnormalslist size"+trianglesNormalsSet.size());
+            normals = new float[normalsList.size()];
+            int i = 0;
+            for(Integer integer : trianglesNormalsSet){
+                normals[3*i] = normalsList.get((integer-1)*3);
+                normals[3*i+1] = normalsList.get((integer-1)*3+1);
+                normals[3*i+2] = normalsList.get((integer-1)*3+2);
+            }
+
+        } else*/ normals = vertexpos;
         return new Mesh(vertexpos, triangles, normals);
     }
 
