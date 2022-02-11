9e570691f9dc25fc49ef895193363761294fb87d
louisforestier <louis.forestier@etu.univ-poitiers.fr>
2022-02-11 10:17:15 +0100

    debut tp5 apr

--- APR/TPs/TP5/student/exo1/student.cpp
+++ APR/TPs/TP5/student/exo1/student.cpp
@@ -14,5 +14,22 @@ void StudentWorkImpl::run_partition_sequential(
 	std::vector<int>& predicate,
 	std::vector<int>& output
 ) {
-	// TODO
+	// TODO	
+	std::vector<int> head_position(input.size());
+	std::vector<int> tail_position(input.size());
+	head_position[0] = 0 ;
+	for ( int i = 0 ; i < input.size() ; i++){
+		if (predicate[i])
+		{
+			head_position[i+1] = 1;
+			tail_position[i] = 0;
+		} 
+		else
+		{
+			head_position[i+1] = 0;
+			tail_position[i] = 1;
+		}
+		int j = 1;		
+		
+	}
 }
