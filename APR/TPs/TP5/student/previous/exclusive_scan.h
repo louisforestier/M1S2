7aa1cad8e3d6c685a1c47bbe8db9923f73b124b1
louisforestier <louis.forestier@etu.univ-poitiers.fr>
2022-02-15 11:20:21 +0100

    ajout patron scan et scatter des exo précédents

--- APR/TPs/TP5/student/previous/exclusive_scan.h
+++ APR/TPs/TP5/student/previous/exclusive_scan.h
@@ -21,14 +21,74 @@ namespace OPP {
         const T Tinit = T(0)
     ) {
         // size of the input
-        const auto fullSize = static_cast<decltype(nbThreads)>(aEnd - aBegin);
+        //const auto fullSize = static_cast<decltype(nbThreads)>(aEnd - aBegin);
         // number of threads (even with small input)
-        const auto realNbThreads = std::min(fullSize, nbThreads);
+        //const auto realNbThreads = std::min(fullSize, nbThreads);
         // chunk size
-        const auto chunkSize = (fullSize + realNbThreads-1) / realNbThreads;
+        //const auto chunkSize = (fullSize + realNbThreads-1) / realNbThreads;
         // barrier
-        Barrier barrier(realNbThreads);
+        //Barrier barrier(realNbThreads);
 
         // TODO
+        int nb_tasks = 4 * OPP::nbThreads;
+        auto fullSize = aEnd - aBegin;
+        auto blockSize = (fullSize + nb_tasks - 1) / nb_tasks;
+
+        OPP::ThreadPool &pool = OPP::getDefaultThreadPool();
+        OPP::Semaphore<uint32_t> semaphore(0);
+        
+        //1ere etape
+        for (int i = 0; i < nb_tasks; ++i)
+        {
+            pool.push_task(
+                [i,Tinit,blockSize, fullSize ,&semaphore,&aBegin,&oBegin,&functor](){
+                    auto blockStart = blockSize * i;
+                    if (blockStart < fullSize)
+                    {
+                        oBegin[blockStart] = Tinit;
+                        for (int j = 1; j < blockSize; j++)
+                        {
+                            if (blockStart+j < fullSize)
+                            {
+                                oBegin[blockStart+j] = functor(oBegin[blockStart+j-1],aBegin[blockStart+j-1]);
+                            }
+                        }
+                    }
+                    semaphore.release();
+                }
+            ); 
+        }
+        semaphore.acquire(nb_tasks);
+         
+        //2eme etape
+        std::vector<T>aux(nb_tasks-1);
+        aux[0] = functor(oBegin[blockSize-1],aBegin[blockSize-1]);
+        for (int i = 1; i < nb_tasks && blockSize * i < fullSize; i++)
+        {
+            aux[i] = functor(functor(aux[i-1],oBegin[blockSize*(i+1)-1]),aBegin[blockSize*(i+1)-1]);
+        }
+
+        //3eme etape
+        for (int i = 0; i < nb_tasks-1; i++)
+        {
+            pool.push_task(
+                [i,blockSize,fullSize,&semaphore,&oBegin,&aux,&functor](){
+
+                    auto blockStart = blockSize * (i+1);
+                    if (blockStart < fullSize)
+                    {
+                        for (int j = 0; j < blockSize ; j++)
+                        {
+                            if (blockStart+j < fullSize)
+                            {
+                                oBegin[blockStart+j] = functor(aux[i], oBegin[blockStart+j]);
+                            }
+                        }
+                    }
+                    semaphore.release();
+                }
+            );
+        }
+        semaphore.acquire(nb_tasks-1);
     }
 };
--- APR/TPs/TP5/student/previous/inclusive_scan.h
+++ APR/TPs/TP5/student/previous/inclusive_scan.h
@@ -20,17 +20,76 @@ namespace OPP {
         const BinaryFunction&& functor // should be associative
     ) {
         // size of the input
-        const auto fullSize = static_cast<decltype(nbThreads)>(aEnd - aBegin);
+        //const auto fullSize = static_cast<decltype(nbThreads)>(aEnd - aBegin);
         // number of threads, even for very small input size
-        const auto realNbThreads = std::min(fullSize, nbThreads);
+        //const auto realNbThreads = std::min(fullSize, nbThreads);
         // chunk size (or block size)
-        const auto chunkSize = (fullSize + realNbThreads-1) / realNbThreads;
+        //const auto chunkSize = (fullSize + realNbThreads-1) / realNbThreads;
         // this allows to store one data per block
-        std::vector<typename OutputIteratorType::value_type> partial_sums(realNbThreads);
+        //std::vector<typename OutputIteratorType::value_type> partial_sums(realNbThreads);
         // barrier to synchronize threads ...
-        Barrier barrier(realNbThreads);
+        //Barrier barrier(realNbThreads);
 
         // TODO ...
+        int nb_tasks = 4 * OPP::nbThreads;
+        auto fullSize = aEnd - aBegin;
+        auto blockSize = (fullSize + nb_tasks - 1) / nb_tasks;
 
+        OPP::ThreadPool &pool = OPP::getDefaultThreadPool();
+        OPP::Semaphore<uint32_t> semaphore(0);
+
+        //1ere etape
+        for (int i = 0; i < nb_tasks; ++i)
+        {
+            pool.push_task(
+                [i,blockSize, fullSize ,&semaphore,&aBegin,&oBegin,&functor](){
+                    auto blockStart = blockSize * i;
+                    if (blockStart < fullSize)
+                    {
+                        oBegin[blockStart] = aBegin[blockStart];
+                        for (int j = 1; j < blockSize; j++)
+                        {
+                            if (blockStart+j < fullSize)
+                            {
+                                oBegin[blockStart+j] = functor(oBegin[blockStart+j-1],aBegin[blockStart+j]);
+                            }
+                        }
+                    }
+                    semaphore.release();
+                }
+            ); 
+        }
+        semaphore.acquire(nb_tasks);
+
+        //2eme etape
+        std::vector<typename OutputIteratorType::value_type>aux(nb_tasks-1);
+        aux[0] = oBegin[blockSize-1];
+        for (int i = 1; i < nb_tasks && blockSize * i < fullSize; i++)
+        {
+            aux[i] = functor(aux[i-1],oBegin[blockSize*(i+1)-1]);
+        }
+
+        //3eme etape
+        for (int i = 0; i < nb_tasks-1; i++)
+        {
+            pool.push_task(
+                [i,blockSize,fullSize,&semaphore,&oBegin,&aux,&functor](){
+
+                    auto blockStart = blockSize * (i+1);
+                    if (blockStart < fullSize)
+                    {
+                        for (int j = 0; j < blockSize ; j++)
+                        {
+                            if (blockStart+j < fullSize)
+                            {
+                                oBegin[blockStart+j] = functor(aux[i], oBegin[blockStart+j]);
+                            }
+                        }
+                    }
+                    semaphore.release();
+                }
+            );
+        }
+        semaphore.acquire(nb_tasks-1);
     }
 };
