0560baa6b1d013b6b47992d74de2da8152bc0112
Louis Forestier <louis.forestier@etu.univ-poitiers.fr>
2022-02-18 19:45:07 +0100

    update todolist

--- A3D/tp1/app/src/main/java/fr/univ_poitiers/dptinfo/algo3d/Sphere.java
+++ A3D/tp1/app/src/main/java/fr/univ_poitiers/dptinfo/algo3d/Sphere.java
@@ -5,7 +5,8 @@ package fr.univ_poitiers.dptinfo.algo3d;
 // - dupliquer les sommets pour donut, cylindre, pipe, pyramid et frustum
 // - normales pas vraiment bonnes pour frustum car pas colinéaires à y, doit calculer la normale selon l'inclinaison provoqué par les différences de rayon
 // - normales pas bonnes non plus pour les faces hautes et basses du frustum et cylindre
-// - attention normals du frustum pas forcément bonne car un des rayons peu ne pas être de rayon 1 donc il faut normaliser le vecteur
+// - attention normals du frustum pas forcément bonne car un des rayons peu ne pas être de rayon 1 donc il faut normaliser le vecteur DONE
+// - modifier l'importeur d'obj pour ne construire les sommets qu'à la fin avec les coordonnées spatiales, les coordonnées de normales et les coordonnées de textures
 // - faire l'atténuation DONE
 // - extraire le GLSL
 // - implanter le modèle blinn phong DONE
